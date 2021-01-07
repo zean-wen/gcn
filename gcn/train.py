2016b1bb8b95b2f14a8551d5c4b25488cd724ebb
Zean Wen <zwen2780@uni.sydney.edu.au>
2021-01-08 00:12:59 +1100

    modified gcn

--- gcn/train.py
+++ gcn/train.py
@@ -18,7 +18,7 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string('dataset', 'cora', 'Dataset string.')  # 'cora', 'citeseer', 'pubmed'
 flags.DEFINE_string('model', 'gcn', 'Model string.')  # 'gcn', 'gcn_cheby', 'dense'
 flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
-flags.DEFINE_integer('epochs', 1000, 'Number of epochs to train.')
+flags.DEFINE_integer('epochs', 300, 'Number of epochs to train.')
 flags.DEFINE_integer('hidden1', 16, 'Number of units in hidden layer 1.')
 flags.DEFINE_float('dropout', 0.2, 'Dropout rate (1 - keep probability).')
 flags.DEFINE_float('weight_decay', 5e-4, 'Weight for L2 loss on embedding matrix.')
@@ -123,7 +123,7 @@ for epoch in range(FLAGS.epochs):
     # if epoch > FLAGS.early_stopping and cost_val[-1] > np.mean(cost_val[-(FLAGS.early_stopping+1):-1]):
     #     print("Early stopping...")
     #     break
-
+print(tf.all_variables())
 print("Optimization Finished!")
 
 # Testing
