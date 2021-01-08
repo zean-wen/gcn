735dfb54c0c5a2c3f6c3a202e4a58194be0aedf5
Zean Wen <zwen2780@uni.sydney.edu.au>
2021-01-08 12:59:12 +1100

    bug

--- gcn/train.py
+++ gcn/train.py
@@ -126,10 +126,12 @@ for epoch in range(FLAGS.epochs):
     #     print("Early stopping...")
     #     break
 
-out = sess.run([model.input_layer.vars['obj_v_proj'], model.input_layer.object_visual_features, model.input_layer.object_embedding], feed_dict=feed_dict)
+out = sess.run([model.input_layer.vars['obj_v_proj'], model.input_layer.object_visual_features, model.layers[0].self.vars['weights_0']], feed_dict=feed_dict)
 print(out[0])
 print(out[0].shape)
 print(out[1])
+print(out[2])
+print(out[2].shape)
 print(tf.all_variables())
 print("Optimization Finished!")
 
