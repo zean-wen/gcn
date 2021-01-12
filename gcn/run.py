41b5a21617aa6bef4356be0edc3c5f4d0c92449a
zean wen <zwen2780@uni.sydney.edu.au>
2021-01-12 14:44:26 +1100

    pach train

--- gcn/run.py
+++ gcn/run.py
@@ -1,13 +1,9 @@
-from __future__ import division
-from __future__ import print_function
-
-import time
+import os
 import json
 import tensorflow as tf
 from tqdm import tqdm
 
-from gcn.utils import *
-from gcn.models import GCN, MLP, GCNModified
+from train import train_model
 
 # Set random seed
 seed = 123
@@ -17,80 +13,24 @@ tf.set_random_seed(seed)
 # Settings
 flags = tf.app.flags
 FLAGS = flags.FLAGS
-flags.DEFINE_string('dataset', 'cora', 'Dataset string.')  # 'cora', 'citeseer', 'pubmed'
-flags.DEFINE_string('model', 'gcn', 'Model string.')  # 'gcn', 'gcn_cheby', 'dense'
-flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
+flags.DEFINE_string('data_dir', './data', 'Data root dir')
+flags.DEFINE_string('save_dir', './genrated_textvqa_gcn_sg', 'dir to save generated scene graph')
 flags.DEFINE_integer('epochs', 300, 'Number of epochs to train.')
-flags.DEFINE_integer('hidden1', 600, 'Number of units in hidden layer 1.')
-flags.DEFINE_float('dropout', 0.2, 'Dropout rate (1 - keep probability).')
-flags.DEFINE_float('weight_decay', 5e-4, 'Weight for L2 loss on embedding matrix.')
-flags.DEFINE_integer('early_stopping', 10, 'Tolerance for early stopping (# of epochs).')
-flags.DEFINE_integer('max_degree', 3, 'Maximum Chebyshev polynomial degree.')
-
-flags.DEFINE_string('data_dir', None, 'Data root dir')
-flags.DEFINE_string('ids_map_dir', None, 'ids_map dir')
-flags.DEFINE_string('tier', 'train', 'train, val, or test')
-flags.DEFINE_bool('use_dummy', False, 'use dummy data for test')
-flags.DEFINE_string('save_dir', None, 'save dir')
+flags.DEFINE_integer('hidden1', 600, 'Number of units in hidden layer, equal to the dim of output scene graph')
+flags.DEFINE_string('tiers', 'train_val_test', 'what tier of textvqa sg to use: train, val, or test')
 flags.DEFINE_integer('start_index', 0, 'image start index')
 
-ids_map_dir = os.path.join(FLAGS.ids_map_dir, '{}_ids_map.json'.format(FLAGS.tier))
-with open(ids_map_dir, 'r') as f:
-    n_images = len(json.load(f)['image_ix_to_id'])
+flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
+flags.DEFINE_float('dropout', 0.2, 'Dropout rate (1 - keep probability).')
+flags.DEFINE_float('weight_decay', 5e-4, 'Weight for L2 loss on embedding matrix.')
+flags.DEFINE_bool('use_dummy', False, 'use dummy data for test')
 
-for image_index in tqdm(range(FLAGS.start_index, n_images),
-                        unit='image',
-                        desc='Generating gcn sg for {}'.format(FLAGS.tier)):
-    adj, object_name_embeddings, object_visual_features, ocr_bounding_boxes, ocr_token_embeddings, y_train, train_mask = \
-        load_data_modified(FLAGS.data_dir, FLAGS.tier, image_index, FLAGS.use_dummy)
+tiers = FLAGS.tiers.split('_')
+for tier in tiers:
+	target_dir = os.path.join(FLAGS.data_dir, 'textvqa_{}'.format(tier), 'targets')
+	n_images = len(os.listdir(target_dir)) 
 
-    support = [preprocess_adj(adj)]
-    num_supports = 1
-    model_func = GCNModified
-
-    placeholders = {
-        'support': [tf.sparse_placeholder(tf.float32) for _ in range(num_supports)],
-        'object_name_embeddings': tf.placeholder(tf.float32, shape=(None, object_name_embeddings.shape[1])),
-        'object_visual_features': tf.placeholder(tf.float32, shape=(None, object_visual_features.shape[1])),
-        'ocr_token_embeddings': tf.placeholder(tf.float32, shape=(None, ocr_token_embeddings.shape[1])),
-        'ocr_bounding_boxes': tf.placeholder(tf.float32, shape=(None, ocr_bounding_boxes.shape[1])),
-        'labels': tf.placeholder(tf.float32, shape=(None, y_train.shape[1])),
-        'labels_mask': tf.placeholder(tf.int32),
-        'dropout': tf.placeholder_with_default(0., shape=()),
-        'num_features_nonzero': tf.placeholder(tf.int32)  # helper variable for sparse dropout
-    }
-
-    # Create model
-    model = model_func(placeholders, input_dim=600, logging=True)
-
-    # Initialize session
-    sess = tf.Session()
-
-    # Init variables
-    sess.run(tf.global_variables_initializer())
-
-    feed_dict = construct_feed_dict(object_name_embeddings, object_visual_features, ocr_bounding_boxes,
-                                    ocr_token_embeddings, support, y_train, train_mask, placeholders)
-
-    # Train model
-    for epoch in range(FLAGS.epochs):
-        # Construct feed dictionary
-        feed_dict = construct_feed_dict(object_name_embeddings, object_visual_features, ocr_bounding_boxes,
-                                        ocr_token_embeddings, support, y_train, train_mask, placeholders)
-        feed_dict.update({placeholders['dropout']: FLAGS.dropout})
-
-        # Training step
-        outs = sess.run([model.opt_op, model.loss, model.accuracy], feed_dict=feed_dict)
-
-    print("Optimization Finished!")
-
-    feed_dict = construct_feed_dict(object_name_embeddings, object_visual_features, ocr_bounding_boxes,
-                                    ocr_token_embeddings, support, y_train, train_mask, placeholders)
-
-    out = sess.run(model.layers[1].hidden_state, feed_dict=feed_dict)
-
-    save_dir = os.path.join(FLAGS.save_dir, '{}_sg_gcn'.format(FLAGS.tier))
-    if not os.path.exists(save_dir):
-        os.mkdir(save_dir)
-    with open(os.path.join(save_dir, '{}.p'.format(image_index)), 'wb') as f:
-        pkl.dump(out, f)
+	for image_index in tqdm(range(FLAGS.start_index, n_images),
+	                        unit='image',
+	                        desc='Generating gcn sg for {}'.format(tier)):
+		train_model(tier, image_index)
--- gcn/train.py
+++ gcn/train.py
@@ -5,141 +5,56 @@ import time
 import tensorflow as tf
 
 from gcn.utils import *
-from gcn.models import GCN, MLP, GCNModified
-
-# Set random seed
-seed = 123
-np.random.seed(seed)
-tf.set_random_seed(seed)
-
-# Settings
-flags = tf.app.flags
-FLAGS = flags.FLAGS
-flags.DEFINE_string('dataset', 'cora', 'Dataset string.')  # 'cora', 'citeseer', 'pubmed'
-flags.DEFINE_string('model', 'gcn', 'Model string.')  # 'gcn', 'gcn_cheby', 'dense'
-flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
-flags.DEFINE_integer('epochs', 300, 'Number of epochs to train.')
-flags.DEFINE_integer('hidden1', 16, 'Number of units in hidden layer 1.')
-flags.DEFINE_float('dropout', 0.2, 'Dropout rate (1 - keep probability).')
-flags.DEFINE_float('weight_decay', 5e-4, 'Weight for L2 loss on embedding matrix.')
-flags.DEFINE_integer('early_stopping', 10, 'Tolerance for early stopping (# of epochs).')
-flags.DEFINE_integer('max_degree', 3, 'Maximum Chebyshev polynomial degree.')
-
-flags.DEFINE_string('data_dir', None, 'Data root dir')
-flags.DEFINE_string('tier', 'train', 'train, val, or test')
-flags.DEFINE_integer('image_index', 0, 'Image Index')
-flags.DEFINE_bool('use_dummy', False, 'use dummy data for test')
+from gcn.models import GCNModified
 
 
-# Load data
-# adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data(FLAGS.dataset)
+def train_model(tier, image_index):
+	adj, object_name_embeddings, object_visual_features, ocr_bounding_boxes, ocr_token_embeddings, y_train, train_mask = \
+		load_data_modified(FLAGS.data_dir, tier, image_index, FLAGS.use_dummy)
 
-adj, object_name_embeddings, object_visual_features, ocr_bounding_boxes, ocr_token_embeddings, y_train, train_mask = \
-    load_data_modified(FLAGS.data_dir, FLAGS.tier, FLAGS.image_index, FLAGS.use_dummy)
+	support = [preprocess_adj(adj)]
+	num_supports = 1
+	model_func = GCNModified
 
-# Some preprocessing
-# features = preprocess_features(features)
+    placeholders = {'support': [tf.sparse_placeholder(tf.float32) for _ in range(num_supports)],
+ 					'object_name_embeddings': tf.placeholder(tf.float32, shape=(None, object_name_embeddings.shape[1])),
+        			'object_visual_features': tf.placeholder(tf.float32, shape=(None, object_visual_features.shape[1])),
+        			'ocr_token_embeddings': tf.placeholder(tf.float32, shape=(None, ocr_token_embeddings.shape[1])),
+        			'ocr_bounding_boxes': tf.placeholder(tf.float32, shape=(None, ocr_bounding_boxes.shape[1])),
+        			'labels': tf.placeholder(tf.float32, shape=(None, y_train.shape[1])),
+        			'labels_mask': tf.placeholder(tf.int32),
+        			'dropout': tf.placeholder_with_default(0., shape=()),
+        			'num_features_nonzero': tf.placeholder(tf.int32)}
 
-if FLAGS.model == 'gcn':
-    support = [preprocess_adj(adj)]
-    num_supports = 1
-    # model_func = GCN
-    model_func = GCNModified
-elif FLAGS.model == 'gcn_cheby':
-    support = chebyshev_polynomials(adj, FLAGS.max_degree)
-    num_supports = 1 + FLAGS.max_degree
-    model_func = GCN
-elif FLAGS.model == 'dense':
-    support = [preprocess_adj(adj)]  # Not used
-    num_supports = 1
-    model_func = MLP
-else:
-    raise ValueError('Invalid argument for model: ' + str(FLAGS.model))
+    # Create model
+    model = model_func(placeholders, input_dim=600, logging=True)
 
-# Define placeholders
-# placeholders = {
-#     'support': [tf.sparse_placeholder(tf.float32) for _ in range(num_supports)],
-#     'features': tf.sparse_placeholder(tf.float32, shape=tf.constant(features[2], dtype=tf.int64)),
-#     'labels': tf.placeholder(tf.float32, shape=(None, y_train.shape[1])),
-#     'labels_mask': tf.placeholder(tf.int32),
-#     'dropout': tf.placeholder_with_default(0., shape=()),
-#     'num_features_nonzero': tf.placeholder(tf.int32)  # helper variable for sparse dropout
-# }
+    # Initialize session
+    sess = tf.Session()
 
+    # Init variables
+    sess.run(tf.global_variables_initializer())
 
-placeholders = {
-    'support': [tf.sparse_placeholder(tf.float32) for _ in range(num_supports)],
-    'object_name_embeddings': tf.placeholder(tf.float32, shape=(None, object_name_embeddings.shape[1])),
-    'object_visual_features': tf.placeholder(tf.float32, shape=(None, object_visual_features.shape[1])),
-    'ocr_token_embeddings': tf.placeholder(tf.float32, shape=(None, ocr_token_embeddings.shape[1])),
-    'ocr_bounding_boxes': tf.placeholder(tf.float32, shape=(None, ocr_bounding_boxes.shape[1])),
-    'labels': tf.placeholder(tf.float32, shape=(None, y_train.shape[1])),
-    'labels_mask': tf.placeholder(tf.int32),
-    'dropout': tf.placeholder_with_default(0., shape=()),
-    'num_features_nonzero': tf.placeholder(tf.int32)  # helper variable for sparse dropout
-}
+    # Train model
+    for epoch in range(FLAGS.epochs):
+        # Construct feed dictionary
+        feed_dict = construct_feed_dict(object_name_embeddings, object_visual_features, ocr_bounding_boxes,
+                                        ocr_token_embeddings, support, y_train, train_mask, placeholders)
+        # add drop out 
+        feed_dict.update({placeholders['dropout']: FLAGS.dropout})
 
-# Create model
-model = model_func(placeholders, input_dim=600, logging=True)
+        # Training step
+        outs = sess.run([model.opt_op, model.loss, model.accuracy], feed_dict=feed_dict)
 
-# Initialize session
-sess = tf.Session()
-
-
-# Define model evaluation function
-def evaluate(features, support, labels, mask, placeholders):
-    t_test = time.time()
-    feed_dict_val = construct_feed_dict(features, support, labels, mask, placeholders)
-    outs_val = sess.run([model.loss, model.accuracy], feed_dict=feed_dict_val)
-    return outs_val[0], outs_val[1], (time.time() - t_test)
-
-
-# Init variables
-sess.run(tf.global_variables_initializer())
-
-feed_dict = construct_feed_dict(object_name_embeddings, object_visual_features, ocr_bounding_boxes,
-                                ocr_token_embeddings, support, y_train, train_mask, placeholders)
-
-cost_val = []
-
-# Train model
-for epoch in range(FLAGS.epochs):
-    t = time.time()
-    # Construct feed dictionary
+    # get 2nd layer hidden state
     feed_dict = construct_feed_dict(object_name_embeddings, object_visual_features, ocr_bounding_boxes,
                                     ocr_token_embeddings, support, y_train, train_mask, placeholders)
-    feed_dict.update({placeholders['dropout']: FLAGS.dropout})
 
-    # Training step
-    outs = sess.run([model.opt_op, model.loss, model.accuracy], feed_dict=feed_dict)
+    out = sess.run(model.layers[1].hidden_state, feed_dict=feed_dict)
 
-    # Validation
-    # cost, acc, duration = evaluate(features, support, y_val, val_mask, placeholders)
-    # cost_val.append(cost)
-
-    # Print results
-    # print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(outs[1]),
-    #       "train_acc=", "{:.5f}".format(outs[2]), "val_loss=", "{:.5f}".format(cost),
-    #       "val_acc=", "{:.5f}".format(acc), "time=", "{:.5f}".format(time.time() - t))
-
-    print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(outs[1]),
-          "train_acc=", "{:.5f}".format(outs[2]), "time=", "{:.5f}".format(time.time() - t))
-
-    # if epoch > FLAGS.early_stopping and cost_val[-1] > np.mean(cost_val[-(FLAGS.early_stopping+1):-1]):
-    #     print("Early stopping...")
-    #     break
-
-print("Optimization Finished!")
-
-feed_dict = construct_feed_dict(object_name_embeddings, object_visual_features, ocr_bounding_boxes,
-                                ocr_token_embeddings, support, y_train, train_mask, placeholders)
-
-out = sess.run(model.layers[1].hidden_state, feed_dict=feed_dict)
-print(out)
-print(out.shape)
-
-
-# Testing
-# test_cost, test_acc, test_duration = evaluate(features, support, y_test, test_mask, placeholders)
-# print("Test set results:", "cost=", "{:.5f}".format(test_cost),
-#       "accuracy=", "{:.5f}".format(test_acc), "time=", "{:.5f}".format(test_duration))
+    # save 2nd layer hidden state
+    save_dir = os.path.join(FLAGS.save_dir, '{}_sg'.format(tier))
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    with open(os.path.join(save_dir, '{}.p'.format(image_index)), 'wb') as f:
+        pkl.dump(out, f)
